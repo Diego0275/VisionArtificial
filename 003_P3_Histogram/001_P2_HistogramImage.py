e3d0244c97a4cd5b2405822b9f9138c6479cf5b2
Diego0275 <a22310275@live.ceti.mx>
2025-05-20 14:00:50 -0600

    Modificacion practica 3
    Se realizó modificación en la practica 3 para que mostrara correctamente el histograma de la imagen original y la imagen ecualizada

--- 003_P3_Histogram/001_P2_HistogramImage.py
+++ 003_P3_Histogram/001_P2_HistogramImage.py
@@ -9,67 +9,46 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Cargar imagen
+img = cv2.imread('Ferrari812.jpg', cv2.IMREAD_GRAYSCALE)
 
-img = cv2.imread('Ferrari812.png', cv2.IMREAD_GRAYSCALE)
-
-
+# Ecualización
 img_eq = cv2.equalizeHist(img)
-
-
+    
+# Histogramas de intensidad
 histx1 = cv2.calcHist([img], [0], None, [256], [0, 256])
 histx2 = cv2.calcHist([img_eq], [0], None, [256], [0, 256])
 
-
+# Histogramas verticales (suma por columna)
 histy1 = np.sum(img, axis=0)
 histy2 = np.sum(img_eq, axis=0)
 
-
+# Plot
 fig = plt.figure(figsize=(14, 6))
 gs = fig.add_gridspec(3, 4)
 
-
 ax1 = fig.add_subplot(gs[0, 0])
 ax1.plot(histx1, color='black')
-ax1.set_title('Histograma X - Original')
+ax1.set_title('Histograma - Original')
 ax1.set_xlim([0, 256])
-ax1.set_xticks([])
-ax1.set_yticks([])
 
-
-ax2 = fig.add_subplot(gs[0, 2])
+ax2 = fig.add_subplot(gs[0, 1])
 ax2.plot(histx2, color='black')
-ax2.set_title('Histograma X - Ecualizada')
+ax2.set_title('Histograma - Ecualizada')
 ax2.set_xlim([0, 256])
-ax2.set_xticks([])
-ax2.set_yticks([])
 
-
-ax3 = fig.add_subplot(gs[1, 0])
-ax3.plot(histy1, color='black')
-ax3.set_title('Histograma Y - Original')
-ax3.set_ylim([0, np.max(histy1)])
-ax3.set_yticks([])
-ax3.set_xticks([])
-
-
-ax4 = fig.add_subplot(gs[1, 1])
+ax4 = fig.add_subplot(gs[1, 0])
 ax4.imshow(img, cmap='gray')
 ax4.set_title('Imagen Original')
 ax4.axis('off')
 
 
-ax5 = fig.add_subplot(gs[1, 2])
-ax5.plot(histy2, color='black')
-ax5.set_title('Histograma Y - Ecualizada')
-ax5.set_ylim([0, np.max(histy2)])
-ax5.set_yticks([])
-ax5.set_xticks([])
-
-
-ax6 = fig.add_subplot(gs[1, 3])
+ax6 = fig.add_subplot(gs[1, 1])
 ax6.imshow(img_eq, cmap='gray')
 ax6.set_title('Imagen Ecualizada')
 ax6.axis('off')
+
+
 
 plt.tight_layout()
 plt.show()
